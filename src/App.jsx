39da885bf16d6e78a401ea30aba54ff3227d2856
anik <anikbhadra01@gmail.com>
2022-07-28 22:25:03 +0530

    setting up the ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <div className="App">
-
+      <h1>Todo List</h1>
+      <div>
+        <input type="text " placeholder='Task..' />
+        <button>Add Task</button>
+        <hr />
+      </div>
     </div>
   )
 }
